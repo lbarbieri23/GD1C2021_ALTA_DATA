0f8e64e0d0913baea43aeca66e500c87cb02474e
lbarbieri23 <38353163+lbarbieri23@users.noreply.github.com>
2021-05-22 02:02:03 -0300

    Update script_creacion_inicial.sql
    Revision y correccion en claves primarias y se agrego las claves foraneas

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -6,11 +6,11 @@ GO
 BEGIN 
 	CREATE TABLE [ALTA_DATA].[PC] (
 	  [id_pc] NVARCHAR PRIMARY KEY,
-	  [id_disco_rigido] NVARCHAR,
-	  [id_memoria_ram] NVARCHAR,
-	  [id_microprocesador] NVARCHAR,
-	  [id_placa_video] INTEGER,
-	  [id_motherboard] INTEGER,
+	  [id_disco_rigido] NVARCHAR FOREIGN KEY REFERENCES [ALTA_DATA].[Disco_Rigido](id_disco_rigido),
+	  [id_memoria_ram] NVARCHAR FOREIGN KEY REFERENCES [ALTA_DATA].[Memoria_Ram](id_memoria_ram),
+	  [id_microprocesador] NVARCHAR FOREIGN KEY REFERENCES [ALTA_DATA].[Microprocesador](id_microprocesador),
+	  [id_placa_video] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[Placa_Video](id_placa_video),
+	  [id_motherboard] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[Motherboard](id_motherboard),
 	  [pc_alto] DECIMAL,
 	  [pc_ancho] DECIMAL,
 	  [pc_profundidad] DECIMAL
@@ -26,9 +26,9 @@ BEGIN
 
 	CREATE TABLE [ALTA_DATA].[Item_factura] (
 	  [id_itemf] INTEGER IDENTITY(1,1) PRIMARY KEY,
-	  [id_factura] INTEGER,
-	  [id_pc] INTEGER,
-	  [id_accesorio] INTEGER,
+	  [id_factura] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[Factura](id_factura),
+	  [id_pc] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[PC](id_pc),
+	  [id_accesorio] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[Accesorios](id_accesorio),
 	  [itemf_cantidad] INTEGER,
 	  [itemf_precio] DECIMAL
 	);
@@ -43,16 +43,16 @@ BEGIN
 
 	CREATE TABLE [ALTA_DATA].[Compra] (
 	  [id_compra] INTEGER IDENTITY(1,1) PRIMARY KEY,
-	  [id_sucursal] INTEGER,
+	  [id_sucursal] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[Sucursal](id_sucursal) ,
 	  [com_fecha] DATETIME2,
 	  [com_total] DECIMAL
 	);
 
 	CREATE TABLE [ALTA_DATA].[Item_compra] (
 	  [id_itemc] INTEGER IDENTITY(1,1) PRIMARY KEY,
-	  [id_compra] INTEGER,
-	  [id_pc] NVARCHAR,
-	  [id_accesorio] DECIMAL,
+	  [id_compra] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[Compra](id_compra),
+	  [id_pc] NVARCHAR FOREIGN KEY REFERENCES [ALTA_DATA].[PC](id_pc),
+	  [id_accesorio] DECIMAL FOREIGN KEY REFERENCES [ALTA_DATA].[Accesorios](id_accesorio),
 	  [itemc_cantidad] INTEGER,
 	  [itemc_precio] DECIMAL
 	);
@@ -72,20 +72,20 @@ BEGIN
 
 	CREATE TABLE [ALTA_DATA].[Stock] (
 	  [id_stock] INTEGER IDENTITY(1,1) PRIMARY KEY,
-	  [id_sucursal] INTEGER,
-	  [id_pc (fk)] NVARCHAR,
-	  [id_accesorio] DECIMAL,
+	  [id_sucursal] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[Sucursal](id_sucursal),
+	  [id_pc (fk)] NVARCHAR FOREIGN KEY REFERENCES [ALTA_DATA].[PC](id_pc),
+	  [id_accesorio] DECIMAL FOREIGN KEY REFERENCES [ALTA_DATA].[Accesorios](id_accesorio),
 	  [stock_cantidad] INTEGER
 	);
 
 	CREATE TABLE [ALTA_DATA].[Accesorios] (
-	  [id_accesorio] DECIMAL,
+	  [id_accesorio] DECIMAL PRIMARY KEY,
 	  [acc_descripcion] NVARCHAR,
 	  [acc_fabricante] NVARCHAR
 	);
 
 	CREATE TABLE [ALTA_DATA].[Disco_Rigido] (
-	  [id_disco_rigido] NVARCHAR,
+	  [id_disco_rigido] NVARCHAR PRIMARY KEY,
 	  [dr_tipo] NVARCHAR,
 	  [dr_capacidad] NVARCHAR,
 	  [dr_fabricante] NVARCHAR,
@@ -120,8 +120,8 @@ BEGIN
 
 	CREATE TABLE [ALTA_DATA].[Factura] (
 	  [id_factura] DECIMAL IDENTITY PRIMARY KEY,
-	  [id_cliente] INTEGER,
-	  [id_sucursal] INTEGER,
+	  [id_cliente] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[Cliente](id_cliente),
+	  [id_sucursal] INTEGER FOREIGN KEY REFERENCES [ALTA_DATA].[Sucursal](id_sucursal),
 	  [fac_fecha] DATETIME2,
 	  [fact_total] DECIMAL
 	);
